a001646b04ce5cd59f31f69c6024e1b180a1b5a6
PikachuUsedSurf <nanaosafokissi@gmail.com>
2024-02-27 09:13:07 +0300

    Added a login button which shows alogin modal and also added more card + dialog modules in the main Content section

--- components/ui/reports.tsx
+++ components/ui/reports.tsx
@@ -14,7 +14,7 @@ export const Reports = () => {
         <div className={"grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4"}>
             <Dialog>
                 <DialogTrigger>
-                    <Card>
+                    <Card className={"hover:bg-gray-50"}>
                         <CardContent className={"flex flex-col"}>
                             <h2 className={"text-lg font-semibold text-left"}>Meeting Notes</h2>
                             <p className={"text-sm text-gray-500 text-left"}>Let us Discuss about the latest project plans for the next phase.</p>
